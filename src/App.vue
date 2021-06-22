a2e01d8a1874d45fe31846ce73198ab591503bbc
enti <enti@student.chalmers.se>
2021-06-22 19:56:10 +0200

    Add vue template from prev project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- Render the content of the current page view -->
+    <navigation />
+  <!-- <div style="background-color:#150135; overflow:hidden;"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+
+export default {
+  components: {
+    Navigation
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100vh;
+  height:100%;
+  overflow:hidden;
+  background-color: #f1f0f5;
+}
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: Items
+    },
+    {
+      // Source : https://techformist.com/create-404-pages-vue-app/
+      path: '*',
+      name: 'notFound',
+      component: NotFound
+    }
+  ]
+})
