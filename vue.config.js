3e7e91f7e7e5812bbabe38297d30c48dc81cfee0
enti <enti@student.chalmers.se>
2021-07-02 11:41:18 +0200

    add

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    configureWebpack: {
-      devtool: 'source-map',
-      publicPath: 'efenaty.github.io'
-    }
-  }
-  
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/hello-world/'
+  : '/'
+}
